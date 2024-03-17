4eda3f0f487e3b675fc17b9c6d9eb0eb9d175168
baothdse <baothdse@gmail.com>
2024-03-17 17:00:17 +0700

    Update code and result

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,7 @@
 # Designate a cloud provider, region, and credentials
 provider "aws" {
-  access_key = "AKIASHG6BKSAQGXSFWEU"
-  secret_key = "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"
+  access_key = 
+  secret_key = 
   region = "us-east-1"
 }
 
@@ -11,7 +11,7 @@ resource "aws_instance" "udacity_t2" {
   ami = "ami-0323c3dd2da7fb37d"
   instance_type = "t2.small"
   tags = {
-    name = var.instance_name_t2_small
+    Name = var.instance_name_t2_small
   }
   subnet_id = var.aws_subnet_public_id
 }
@@ -22,7 +22,7 @@ resource "aws_instance" "udacity_m4" {
   ami = "ami-0323c3dd2da7fb37d"
   instance_type = "m4.large"
   tags = {
-    name = var.instance_name_m4_large
+    Name = var.instance_name_m4_large
   }
   subnet_id = var.aws_subnet_public_id
 }
